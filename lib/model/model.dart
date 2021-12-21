3f55714dd8c327492d41d888b6f8831cc57cbddc
Bidyut mazumder <46864109+bidyut102@users.noreply.github.com>
2021-12-21 13:42:23 +0600

    update

--- lib/model/model.dart
+++ lib/model/model.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
 
-class Data{
+class ProductData{
   double? size;
   String? name;
   String? des;
   String? img;
   bool? is_fav;
-
+  ProductData({required this.size, required this.name, required this.des, required this.img, required this.is_fav});
 }
 
+List<ProductData> products = [
+  ProductData(
+    size: 10.5,
+    name: "Shoes",
+    des: "Bangladeshi Shoes",
+    img: "",
+    is_fav: false,
+  ),
+  ProductData(
+    size: 7,
+    name: "Shoes",
+    des: "Bangladeshi Shoes",
+    img: "",
+    is_fav: false,
+  ),
+  ProductData(
+    size: 9,
+    name: "Shoes",
+    des: "Bangladeshi Shoes",
+    img: "",
+    is_fav: false,
+  ),
+];
